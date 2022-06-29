29dccd82246d4d6edbac4429fc478843cd6aa13b
khaled mohamed <khaledmohamed0140@gmail.com>
2022-06-29 16:22:48 +0200

    updating routing navBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import State from './component/state';
+import NavBar from './component/navBar';
+import Home from './component/home/home';
+import Shop from './component/shop';
+import About from './component/about';
+import ProductDetails from './component/shop/productDetails';
+import {BrowserRouter , Route , Routes} from 'react-router-dom'
 
-function App() {
-
-   
+function App() {   
   return (
-    <div class="text-center" className="App">
-     <State/>
-    </div>
+
+    <BrowserRouter>
+     <NavBar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/state' element={<State/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/shop' element={<Shop/>}/>
+      <Route path='/product/:id' element={<ProductDetails/>}/>
+     </Routes>
+   </BrowserRouter>
+
   );
 }
 
